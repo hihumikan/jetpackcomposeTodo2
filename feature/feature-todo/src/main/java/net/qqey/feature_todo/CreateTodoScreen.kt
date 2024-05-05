016dd32776bf502d3692fe67fcc4251f9f81c4cc
hihumikan <a@qqey.net>
2024-05-06 04:49:57 +0900

    #5.1

--- feature/feature-todo/src/main/java/net/qqey/feature_todo/CreateTodoScreen.kt
+++ feature/feature-todo/src/main/java/net/qqey/feature_todo/CreateTodoScreen.kt
@@ -1,9 +1,55 @@
 package net.qqey.feature_todo
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CreateTodoScreen() {
-    Text("CreateTodoScreen")
+fun CreateTodoScreen(
+    back: () -> Unit,
+) {
+    var title by rememberSaveable { mutableStateOf("") }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(stringResource(R.string.create_todo_title))},
+                navigationIcon = {
+                    IconButton(onClick = back) {
+                        Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
+                    }
+                },
+            )
+        }
+    ) {
+        paddingValues ->
+        Column(
+            modifier = Modifier.padding(paddingValues)
+        ) {
+            Text("CreateTodoScreen")
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun Preview(){
+    CreateTodoScreen(
+        back = {}
+    )
 }
